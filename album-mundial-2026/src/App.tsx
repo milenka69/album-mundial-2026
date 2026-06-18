b156f5977b95fbd77fb005459c1e710556867842
Milenka Castillo <Milenka.castillo@inacapmail.cl>
2026-06-18 09:47:34 -0400

    aa

--- album-mundial-2026/src/App.tsx
+++ album-mundial-2026/src/App.tsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { stickers } from './data/stickers'
+
+console.log('stickers:', stickers)
 
 function App() {
   const [count, setCount] = useState(0)
